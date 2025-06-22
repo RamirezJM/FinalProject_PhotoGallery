3e50ddd1faa5109548e348f62bf5e425396da8ac
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-06-21 23:11:31 -0400

    style adjustment, js upgrade

--- js/script.js
+++ js/script.js
@@ -1,35 +1,38 @@
-<<<<<<< HEAD
+
+const imageContainer = document.getElementById('image')
+const infoText = `<p>Hover over an image below to display here.</p>`
+imageContainer.innerHTML = infoText
 
 /* ---ONMOUSEOVER--- */
 
 function upDate(previewPic) {
 
-  document.getElementById('image').innerHTML = previewPic.alt;
-  document.getElementById('image').style.backgroundImage = `url('${previewPic.src}')`
+ imageContainer.innerHTML = `<p class="character-name">${previewPic.alt}</p>`
+ imageContainer.style.backgroundImage = `url('${previewPic.src}')`
 }
 
 /* ---ONMOUSEOUT--- */
 
 function unDo() {
 
-  document.getElementById('image').innerHTML = "Hover over an image below to display here."
-  document.getElementById('image').style.backgroundImage = "none"
+  imageContainer.innerHTML = infoText
+  imageContainer.style.backgroundImage = "none"
 }
 
 /* ---ONFOCUS--- */
 
 function focusFunction(previewPic) {
 
-  document.getElementById('image').innerHTML = previewPic.alt;
-  document.getElementById('image').style.backgroundImage = `url('${previewPic.src}')`
+  imageContainer.innerHTML = `<p>${previewPic.alt}</p>`
+  imageContainer.style.backgroundImage = `url('${previewPic.src}')`
 }
 
 /* ---ONBLUR--- */
 
 function blurFunction() {
   
-  document.getElementById('image').innerHTML = "Hover over an image below to display here."
-  document.getElementById('image').style.backgroundImage = "none"
+  imageContainer.innerHTML = infoText
+  imageContainer.style.backgroundImage = "none"
 }
 
 /* ---ADDING TABFOCUS--- */
@@ -39,46 +42,4 @@ function addTabFocus() {
   for (let i = 0; i < myImages.length; i++) {
     myImages[i].setAttribute("tabindex", "0")
   }
-=======
-
-/* ---ONMOUSEOVER--- */
-
-function upDate(previewPic) {
-
-  document.getElementById('image').innerHTML = previewPic.alt;
-  document.getElementById('image').style.backgroundImage = `url('${previewPic.src}')`
-}
-
-/* ---ONMOUSEOUT--- */
-
-function unDo() {
-
-  document.getElementById('image').innerHTML = "Hover over an image below to display here."
-  document.getElementById('image').style.backgroundImage = "none"
-}
-
-/* ---ONFOCUS--- */
-
-function focusFunction(previewPic) {
-
-  document.getElementById('image').innerHTML = previewPic.alt;
-  document.getElementById('image').style.backgroundImage = `url('${previewPic.src}')`
-}
-
-/* ---ONBLUR--- */
-
-function blurFunction() {
-  
-  document.getElementById('image').innerHTML = "Hover over an image below to display here."
-  document.getElementById('image').style.backgroundImage = "none"
-}
-
-/* ---ADDING TABFOCUS--- */
-
-function addTabFocus() {
-  let myImages = document.querySelectorAll("img")
-  for (let i = 0; i < myImages.length; i++) {
-    myImages[i].setAttribute("tabindex", "0")
-  }
->>>>>>> dfc6e14b1290c8dc94d94e38cd3b8632ff242c55
 }
